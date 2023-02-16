749a28031a7fb0fa2d602a935879fe63d4dcbfc5
myconmatsu <maikon.109@gmail.com>
2023-02-16 18:55:18 +0900

    バリデーション実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,9 @@ class User < ApplicationRecord
   has_many :orders
 
   validates :nickname, presence: true
+  validates :first_name, presence: true
+  validates :family_name, presence: true
+  validates :first_name_kana, presence: true
+  validates :family_name_kana, presence: true
+  validates :birth_day, presence: true
 end

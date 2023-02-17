95c5071b5252add9a411c2c56e5b9e91626a5344
myconmatsu <maikon.109@gmail.com>
2023-02-17 21:17:53 +0900

    バリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :items
-  has_many :orders
+  # has_many :items
+  # has_many :orders
 
   validates :nickname, presence: true
   validates :first_name, presence: true
@@ -12,4 +12,14 @@ class User < ApplicationRecord
   validates :first_name_kana, presence: true
   validates :family_name_kana, presence: true
   validates :birth_day, presence: true
+
+  validates :password, presence: true, length: { minimum: 6 }
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
+  
+  validates :family_name,            presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ } 
+  validates :family_name_kana,       presence: true, format: {with: /\A[ァ-ヶー－]+\z/ } 
+  validates :first_name,             presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ } 
+  validates :first_name_kana,        presence: true, format: {with: /\A[ァ-ヶー－]+\z/ } 
+
 end

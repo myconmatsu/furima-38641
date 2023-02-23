e1ee7d458ca71845a435b772cc2b4b2568707cf4
myconmatsu <maikon.109@gmail.com>
2023-02-23 16:42:34 +0900

    選択肢の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,27 @@
 class Item < ApplicationRecord
-belongs_to : user
-has_one : order
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
+  belongs_to :user
+  belongs_to :condition
+  belongs_to :deliveryarea
+  belongs_to :category
+  belongs_to :shipmentday
+  belongs_to :prefecture
+
+  # has_one : order
+  has_one_attached :image
+  
+
+  with_options  presence: true do
+    validates        :item_name
+    validates        :content
+    validates        :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+    validates        :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
+    validates        :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+    validates        :delivery_area_id, numericality: { other_than: 1 , message: "can't be blank"}
+    validates        :shipment_day_id, numericality: { other_than: 1 , message: "can't be blank"}
+    validates        :item_price
+    validates        :image
+  end
+
 end

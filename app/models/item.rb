407d625a97cab5e1ebad8afbbb2b0b951723d542
myconmatsu <maikon.109@gmail.com>
2023-02-25 15:58:58 +0900

    テストコード実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,16 +11,21 @@ class Item < ApplicationRecord
   # has_one : order
   has_one_attached :image
   
+  def was_attached?
+    self.image.attached?
+  end
+
 
   with_options  presence: true do
-    validates        :item_name
+    validates        :item_name 
     validates        :content
-    validates        :category_id, numericality: { other_than: 1 , message: "can't be blank"}
-    validates        :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
-    validates        :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
-    validates        :delivery_area_id, numericality: { other_than: 1 , message: "can't be blank"}
-    validates        :shipment_day_id, numericality: { other_than: 1 , message: "can't be blank"}
-    validates        :item_price
+    validates        :category_id, numericality: { other_than: 1 }
+    validates        :condition_id, numericality: { other_than: 1 }
+    validates        :prefecture_id, numericality: { other_than: 1 }
+    validates        :delivery_area_id, numericality: { other_than: 1 }
+    validates        :shipment_day_id, numericality: { other_than: 1 }
+    validates        :item_price,numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 } 
+    validates        :item_price,numericality: { only_integer: true }
     validates        :image
   end
 

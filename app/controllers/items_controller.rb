65dfc6512b292c818fd64f7cbf3b1c0fb9082ab9
myconmatsu <maikon.109@gmail.com>
2023-03-02 21:09:01 +0900

    重複コードまとめ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index,:show]
   before_action :move_to_index, only: [:edit,:update]
+  before_action :item_set, only: [:show,:edit,:update]
+
   def index
     @items = Item.order('created_at DESC')
   end
@@ -19,15 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -40,6 +39,10 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:item_name, :content, :category_id, :condition_id, :prefecture_id, :delivery_area_id, :shipment_day_id, :item_price, :image).merge(user_id: current_user.id)
+  end
+
+  def item_set
+    @item = Item.find(params[:id])
   end
 
   def move_to_index

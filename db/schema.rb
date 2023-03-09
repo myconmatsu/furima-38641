9b358109f23178bf4e67edd153e0aacef1618e09
myconmatsu <maikon.109@gmail.com>
2023-03-09 13:36:09 +0900

    購入のテスト実装

--- db/schema.rb
+++ db/schema.rb
@@ -61,7 +61,7 @@ ActiveRecord::Schema.define(version: 2023_03_05_062406) do
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
-  create_table "order_buyer", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "order_buyers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
--- spec/factories/order_addresses.rb
+++
@@ -1,5 +0,0 @@
-FactoryBot.define do
-  factory :order_address do
-    
-  end
-end
